6ac26bcd242077c78e0db3f6539d1580176179f3
Artem Shevchenko <artem@Artems-Mac-mini.local>
2022-01-18 12:07:12 +0200

    Changed button types to correct values

--- src/components/styled/SubmitButton.styled.js
+++ src/components/styled/SubmitButton.styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const StyledButton = styled.button`
+export const StyledButton = styled.button.attrs({ type: "button" })`
   align-self: flex-start;
   display: block;
   border: 0px solid transparent;
@@ -25,6 +25,8 @@ export const StyledButton = styled.button`
   }
 `;
 
-export const StyledSubmitButton = styled(StyledButton)`
+export const StyledSubmitButton = styled(StyledButton).attrs({
+  type: "submit",
+})`
   margin-top: 32px;
 `;
